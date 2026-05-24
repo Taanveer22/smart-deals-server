32575eea99ad19da0bc861369dd874e916865deb
Taanveer22 <taanveer469@gmail.com>
2026-05-25 03:52:51 +0600

    alhamdulliah server side setup done

---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// instance
+const app = express();
+const port = process.env.PORT || 5000;
+
+// database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.89rnkti.mongodb.net/?appName=Cluster0`;
+// console.log(uri);
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+    console.log('finished client work');
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello from smart deals..');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
